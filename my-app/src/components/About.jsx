0cc4bf954a6f001feafbc843e3f19dd6779a819c
Nate Falconer <falconerresources@gmail.com>
2020-02-27 09:36:09 -0500

    finishing up

--- my-app/src/components/About.jsx
+++ my-app/src/components/About.jsx
@@ -5,7 +5,16 @@ class About extends Component {
         return (
             <div className="aboot">
             <NavBar />
-            <p>lolooool</p>
+            <div className="abtext">
+            <h2>Swansonews is a single-page application built in React.</h2>
+            <br />
+            <p>It works by fetching a random quote from a Ron Swanson quote API, then filtering the quote into searchable terms and plugging those terms into a search in The Guardian's API. It then scrapes the news article data and formats the text so the user can read one of three articles without ever leaving Swansonews.</p>
+            <p>This project was developed by Nate Falconer.</p>
+            <a href="https://github.com/jamesseanwright/ron-swanson-quotes">Ron Swanson Quotes API</a> <br />
+            <a href="https://open-platform.theguardian.com/">The Guardian Open Platform</a> <br />
+            <a href="https://github.com/NateFalconer">My GitHub</a>
+            </div>
+            <img className="pyr" alt="pyra" src="./assets/pyramid.png" />
             </div>
         );
     }
---
+++ readme.md
@@ -0,0 +1,1 @@
+Swansonews by Nate Falconer
